e2ccce40b3c4fa78cd668a0187259ae03dd61686
Aaron Boakye <cheminko@hotmail.com>
2026-05-23 23:42:13 +0100

    problem set 3/2 add taquira solution

---
+++ pset3/taqueria.py
@@ -0,0 +1,56 @@
+# One of the most popular places to eat in Harvard Square is Felipe’s Taqueria, which offers a menu of entrees, per the dict below,
+# wherein the value of each key is a price in dollars:
+
+# {
+#     "Baja Taco": 4.25,
+#     "Burrito": 7.50,
+#     "Bowl": 8.50,
+#     "Nachos": 11.00,
+#     "Quesadilla": 8.50,
+#     "Super Burrito": 8.50,
+#     "Super Quesadilla": 9.50,
+#     "Taco": 3.00,
+#     "Tortilla Salad": 8.00
+# }
+
+# In a file called taqueria.py, implement a program that enables a user to place an order, prompting them for items, one per line,
+# until the user inputs control-d (which is a common way of ending one’s input to a program).
+# After each inputted item, display the total cost of all items inputted thus far, prefixed with a dollar sign ($)
+# and formatted to two decimal places. Treat the user’s input case insensitively. Ignore any input that isn’t an item.
+# Assume that every item on the menu will be titlecased.
+
+#pseudocode
+# may need while loop for reprompting user
+# initialise running total var set to 0
+# capture user input in item var (title())
+# check item membership
+# add price to running total
+# print total with dollar sign prefixed
+
+menu = {
+    "Baja Taco": 4.25,
+    "Burrito": 7.50,
+    "Bowl": 8.50,
+    "Nachos": 11.00,
+    "Quesadilla": 8.50,
+    "Super Burrito": 8.50,
+    "Super Quesadilla": 9.50,
+    "Taco": 3.00,
+    "Tortilla Salad": 8.00
+}
+
+
+total = 0
+
+while True:
+    try:
+        item = input("Item: ").title()
+        if item not in menu:
+            continue
+    except EOFError:
+        break
+    except KeyError:
+        pass
+    else:
+        total += menu[item]
+        print(f"Total: ${total:.2f}")
